599e957c7a4ea3ac6e88cb1a742524ce5bb01c03
Uri Goldstein <uri.goldstein@gmail.com>
2018-01-31 15:12:29 +0200

    Renamed ut files to tests.py

--- task1/tests.py
+++ task1/tests.py
@@ -6,7 +6,7 @@ class task1tests(unittest.TestCase):
     def test_helloWorld(self):
         expected = 'Hello, World!'
         actual = task1.helloWorld()
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
--- task3/tests.py
+++ task3/tests.py
@@ -6,14 +6,7 @@ class task3tests(unittest.TestCase):
     def test_pi(self):
         expected = 3.1415926
         actual = truncate(task3.pi(), 7)
-        self.assertEquals(actual, expected)
-'''
-    def test_truncate1(self):
-        self.assertEquals(3.1415926, task3.truncate(3.141592611111, 7))
-
-    def test_truncate2(self):
-        self.assertEquals(3.1415926, task3.truncate(3.141592699999, 7))
-'''
+        self.assertEqual(actual, expected)
 
 def truncate(num, digits):
     return int(num * 10**digits) / 10**digits
